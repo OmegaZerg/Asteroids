01468fe9749282837b4603a1ec54b7acaa83788b
OmegaZerg <ragged17@Bellsouth.net>
2025-03-15 15:11:33 -0500

    add initial game code to main, added circleshape.py file with basic class logic.

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from constants import *
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    fps_clock = pygame.time.Clock()
+    dt = 0
 
     loop = True
 
@@ -16,6 +15,8 @@ def main():
                 return
         screen.fill(000)
         pygame.display.flip()
+        #limit the framerate to 60 FPS
+        dt = fps_clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
