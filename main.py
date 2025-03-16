09aea6b97c7c27fa26e1f6ebb578e1961a6fd839
OmegaZerg <ragged17@Bellsouth.net>
2025-03-16 10:57:16 -0500

    created new asteroidfield and asteroid files with required logic. Updated main and circleshape file logic for game to use asteroids properly and end when collision occurs.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,8 +12,12 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group() # holds objects that need to be updated each frame
     drawable = pygame.sprite.Group() # holds objects that need to be drawn each frame
+    asteroids = pygame.sprite.Group() # holds the asteroid objects
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    asteroid_field = AsteroidField()
 
     loop = True
 
@@ -26,7 +33,11 @@ def main():
         for obj in drawable:
             obj.draw(screen)
         
-        
+        # Check for collisions
+        for obj in asteroids:
+            if player.is_collision(obj):
+                sys.exit("Game over man!")
+
         pygame.display.flip()
         
         #limit the framerate to 60 FPS
